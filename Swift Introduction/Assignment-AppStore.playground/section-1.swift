d0e44ea98481d7af82f67d368ec30f95ccdeca8a
Janusz Chudzynski <appzzman>
2015-01-29 15:13:37 -0600

    Minor changes

---
+++ Swift Introduction/Assignment-AppStore.playground/section-1.swift
@@ -0,0 +1,81 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+
+/*ADVANCED Max 100% or A*/
+//Create a Swift playground with a model that simulates how AppStore works.
+/*
+    Model should consists of three classes
+    Developers
+    Apps
+    AppStore
+
+    App store will have following rules:
+        Developers have following properties and capabalities:
+            they have unique id (Int)
+            they can create apps
+            they can transfer apps to other developer, which means that they can receive apps from other developers as well
+            they can submit apps to the AppStore
+            they own a bank account that is presented as profit (Double)
+
+    Apps have following properties:
+        developer (owner of the application)
+        name (name of the app)
+        price (price of the app)
+
+    AppStore have following properties
+        apps (collection ofthe apps submitted and approved to the app store)
+        profit variable that stores profit of the store
+        submission process (developers can submit apps to the store but they are not always accepted. Use an arc4random_uniform function to randomize the submission process
+
+        every day customers buy apps from the App Store. Use an arc4random_uniform function to choose randomly which apps will be sold on the store and how many apps are sold. When the apps are sold app store receives 30% of the revenue and 70 percent of the revenue goes to the developer.
+
+
+        every day it will computes number of sold copies and send information to developer
+
+        when developer submits the app to the store, and submission is successful AppStore should update its app listings represented as array of apps
+
+    
+    Scoring:
+    
+    Valid structure of classes (50%)
+    Submission process (10%)
+    Randomized process of selling apps(10%)
+    Transferring apps between developers (10%)
+    Testing behavior of the store (20%)
+
+*/
+
+
+
+class AppStore
+{
+    
+}
+
+
+class App{
+
+
+}
+
+
+class Developer{
+
+}
+
+
+/*ADVANCED Max 80% or B*/
+
+
+
+
+
+
+
+
+
+
+
+
