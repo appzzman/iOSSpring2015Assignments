348222ec439643f4d19d82c938a4b172ccb9b2c7
Janusz Chudzynski <appzzman>
2015-02-25 18:07:56 -0600

    Added annotations and delegate methods

--- Maps/UWFMap/UWFMap/LocationsModel.swift
+++ Maps/UWFMap/UWFMap/LocationsModel.swift
@@ -11,8 +11,8 @@ import Foundation
 
 
 class Location{
-    var image1 = ""
-    var image2 = ""
+    var image1 = "warsaw1.jpg"
+    var image2 = "warsaw2.jpg"
     var description = "Warsaw, is the capital and largest city of Poland. It is located on the Vistula River, in east-central Poland. Its population is estimated at 1.729 million residents within a greater metropolitan area of 2.666 million residents, which makes Warsaw the 9th most populous capital city in the European Union. The first historical reference to Warsaw dates back to the year 900/1000 (9th/10th) century."
     var latitude = 52.233
     var longitude = 21.0167
--- Maps/UWFMap/UWFMap/ViewController.swift
+++ Maps/UWFMap/UWFMap/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UIViewController, MKMapViewDelegate {
 //TODO: add an iBOutlet for the mapview ( connect it with Storyboard)
     @IBOutlet weak var mapView: MKMapView!
     var locationManager = CLLocationManager()
+    var annotations = [MKPointAnnotation]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 //TODO: set the delegate of the map view here
@@ -23,16 +25,97 @@ class ViewController: UIViewController, MKMapViewDelegate {
 //TODO: enable showing user's location
         self.mapView.showsUserLocation = true
         self.locationManager.requestWhenInUseAuthorization()
-
+        
     }
+    
+    @IBAction func segmentedControlChanged(sender: AnyObject) {
+        self.addAnnotations()
+        
+    }
+    
+    
+    func mapView(mapView: MKMapView!, regionDidChangeAnimated animated: Bool)
+    {
+        println("changed")
+    }
+    
+    func addAnnotations(){
+      
+        var annotation: MKPointAnnotation = MKPointAnnotation()
+        annotation.coordinate = CLLocationCoordinate2D(latitude: 52.233, longitude: 21.0167)
+        
+        var annotation2: MKPointAnnotation = MKPointAnnotation()
+        annotation2.coordinate = CLLocationCoordinate2D(latitude: 42.233, longitude: 11.0167)
+        
+        self.zoomOnLocation(annotation.coordinate)
+        
+        annotation.title = "Warsaw"
+        annotation2.title = "Test"
+ 
+        // annotation.
+        self.annotations.append(annotation)
+        self.annotations.append(annotation2)
+        self.mapView.addAnnotation(annotation)
+        self.mapView.addAnnotation(annotation2)
+      
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    func zoomOnLocation(location:CLLocationCoordinate2D){
+        var span = MKCoordinateSpan(latitudeDelta: 0.0107872, longitudeDelta: 0.0909863)
+        var newRegion: MKCoordinateRegion = MKCoordinateRegion(center: location, span: span)
+         self.mapView.setRegion(newRegion, animated: true)
+    }
+    
+    
 //TODO: Finish implementation of method that will show user's current location
+   @IBAction func showCurrentLocation(){
+    var cl2d = CLLocationCoordinate2D(latitude: self.mapView.userLocation.coordinate.latitude, longitude: self.mapView.userLocation.coordinate.longitude)
+
+    var span = MKCoordinateSpan(latitudeDelta: 0.0007872, longitudeDelta: 0.0109863)
+    var newRegion: MKCoordinateRegion = MKCoordinateRegion(center: cl2d, span: span)
+    self.mapView.setRegion(newRegion, animated: true)
+    
+    }
+    
+    func mapView(mapView: MKMapView!, viewForAnnotation annotation: MKAnnotation!) -> MKAnnotationView!{
+        if annotation is MKUserLocation {
+            //return nil so map view draws "blue dot" for standard user location
+            return nil
+        }
+        
+        
+        let reuseid = "pinid"
+    
+        var mp = mapView.dequeueReusableAnnotationViewWithIdentifier(reuseid)
+
+        if mp == nil {
+            mp = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseid)
+            mp.canShowCallout = true
+            var b: UIButton = UIButton.buttonWithType(UIButtonType.DetailDisclosure) as UIButton
+            mp.rightCalloutAccessoryView = b
+            
+        }
+        mp.annotation = annotation
+        
+        return mp
+    }
     
     
+        
 }
+
+
+
+
+
+
+
+
+
 
